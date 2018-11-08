981aae41ad70fa8fd226a6b71a3bdda5ed7520b9
Oliver Gomes <ogomesphoto@gmail.com>
2018-11-08 16:32:34 -0500

    modal imported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import ModalComponent from "./components/Modal";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <ModalComponent />
       </div>
     );
   }
